5ddfc23a9c6ef21fc43907a095a1b54161ae2872
Tejar Maulana <142279980+Tejar456@users.noreply.github.com>
2025-01-02 20:14:11 +0700

    update components

--- components/Kerjasama.vue
+++ components/Kerjasama.vue
@@ -11,32 +11,32 @@
     <div  data-aos="fade-up" class="flex justify-center gap-x-6 sm:gap-x-12 flex-wrap lg:gap-x-24">
       <img
         class="py-3 lg:py-5 w-16 h-auto md:w-24 lg:w-28 mx-auto sm:mx-0 object-contain"
-        src="/assets/img/kerjasama/agate.png"
+        src="~/assets/img/kerjasama/agate.png"
         alt="Logo agate"
       />
       <img
         class="py-3 lg:py-5 w-16 h-auto md:w-24 lg:w-28 mx-auto sm:mx-0 object-contain"
-        src="/assets/img/kerjasama/ahm.png"
+        src="~/assets/img/kerjasama/ahm.png"
         alt="Logo ahm"
       />
       <img
         class="py-3 lg:py-5 w-16 h-auto md:w-24 lg:w-28 mx-auto sm:mx-0 object-contain"
-        src="/assets/img/kerjasama/skyline.png"
+        src="~/assets/img/kerjasama/skyline.png"
         alt="Logo skyline"
       />
       <img
         class="py-3 lg:py-5 w-16 h-auto md:w-24 lg:w-28 mx-auto sm:mx-0 object-contain"
-        src="/assets/img/kerjasama/sti.png"
+        src="~/assets/img/kerjasama/sti.png"
         alt="Logo sti"
       />
       <img
         class="py-3 lg:py-5 w-16 h-auto md:w-24 lg:w-28 mx-auto sm:mx-0 object-contain"
-        src="/assets/img/kerjasama/cikara.png"
+        src="~/assets/img/kerjasama/cikara.png"
         alt="Logo cikara"
       />
       <img
         class="py-3 lg:py-5 w-16 h-auto md:w-24 lg:w-28 mx-auto sm:mx-0 object-contain"
-        src="/assets/img/kerjasama/inovindo.png"
+        src="~/assets/img/kerjasama/inovindo.png"
         alt="Logo inovindo"
       />
     </div>
